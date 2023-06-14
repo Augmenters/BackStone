d9ecba3bb913f996bba3090dd27044a53adaccdd
Andrew Brain <andrewbrain2019@gmail.com>
2023-06-14 07:10:24 -0500

    Start celery worker in a docker container
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- Python/CrimeData/celery_app.py
+++ Python/CrimeData/celery_app.py
@@ -1,7 +1,12 @@
+import os
+
 from celery import Celery
 
-BROKER_URL = 'redis://localhost:6380/0'
-BACKEND_URL = 'redis://localhost:6380/1'
+host = os.environ.get('REDIS_HOST', 'localhost')
+port = os.environ.get('REDIS_PORT', 6379)
+
+BROKER_URL = f'redis://{host}:{port}/0'
+BACKEND_URL = f'redis://{host}:{port}/1'
 tasks = ['tasks']
 
 celery_app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL, include=tasks)
